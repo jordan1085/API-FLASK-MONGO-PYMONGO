08f0806d26e37576d6a8f1098a14efcc6bffe672
Jordan <jordanhernando1085@gmail.com>
2021-01-15 19:31:06 -0300

    Cong server y mongo

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+#conexion
+app.config["MONGO_URI"] = "mongodb://localhost:27017/apipython"
+mongo = PyMongo(app) #pasamos la confi
 
 if __name__ == "__main__":
     app.run(debug=True)
