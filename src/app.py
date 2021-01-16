95bb55036a7bc5dcc22c03e66ca7f9bfc762ef8e
Jordan <jordanhernando1085@gmail.com>
2021-01-15 21:58:18 -0300

    get de usuario por id

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo #modulo que ya integra flask y mongo
 from werkzeug.security import generate_password_hash, check_password_hash #modulo para hashear pass
 from bson import json_util #modulo para combertir de bson a json
+from bson.objectid import ObjectId #comvertir el string de id a un object id
 
 app = Flask(__name__)
 
@@ -49,7 +50,14 @@ def get_users():
     response = json_util.dumps(users) #bson a json
 
     return Response(response, mimetype='application/json') #Response permite inprimir formateado el json
-    
+
+#Ruta para mostrar usuario por ID
+@app.route('/usuarios/<id>', methods={'GET'})
+def get_user(id):
+    user = mongo.db.users.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(user)
+
+    return Response(response, mimetype='application/json')
 
 #Error 404
 @app.errorhandler(404)
